32d1eafb59e969e7778e400ab9db8b70ed5ca63c
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-04 16:00:59 -0500

    Created an R function to process all chromosomes for a given sample using the categorizeAD.sh shell script

--- R/genSnpZygosity.R
+++ R/genSnpZygosity.R
@@ -1,23 +1,40 @@
-#' Title
-#' @description 
+#' Simplifies the SNP Zygosity from MuTect
+#' 
+#' @description Takes a single sample Mutect VCF that has force called all dbSNP
+#' sites and reduces it to a single column of 1,2,3 values corresponding to 
+#' REF.HOMOZYGOUS, HETEROZYGOUS, and ALT.HOMOZYGOUS respectively using Perl. 
+#' If multiple samples are given, it will concatenate the samples into a single matrix.
+#' 
+#' @param sampleid Vector of sample IDs for the filename [chr].[sampleid].vcf
+#' @param chrs Chrs for the filename [chr].[sampleid].vcf
 #' 
 #' @return
 #' @export
-#'
-#' @examples
 getSnpZygosity <- function(sampleid, chrs = paste0("chr", c(1:22, "X", "Y"))){
-  sampleid <- 'NET-2-001b_02_T_DNA.processed'
-  
-  sapply(chrs, function(chr){
-    vcf_file <- paste0(chr, sampleid, ".vcf")
-    
-  })
-  
+  # sampleid <- 'NET-2-001b_02_T_DNA.processed'
+  sampleid_dir <- gsub("[^a-zA-Z0-9]", "", sampleid)
+  dir.create(sampleid_dir, showWarnings = FALSE)
   
   self_pkg_name <- 'WadingPool'
   bin_dir <- system.file("bin/", package = self_pkg_name)
-  conv.cmd <- paste0(bin_dir, "/categorizeAD.sh", 
-                     " -v ", vcf_file, 
-                     " -o ", out_file)
-  conv.cmd.res <- try(system(command = conv.cmd, intern = TRUE))
+  
+  categorize_results <- sapply(chrs, function(chr){
+    vcf_file <- paste0(chr, ".", sampleid, ".vcf")
+    out_file <- file.path(sampleid_dir, paste0(chr, ".", sampleid, "_out.tsv"))
+    
+    ## Executes the categorizeAD.sh shell script that parses the single-sample
+    #  MuTect VCF output 
+    categorize_cmd <- paste0("sh ", bin_dir, "/categorizeAD.sh", 
+                             " -v ", vcf_file, 
+                             " -o ", out_file)
+    categorize_res <- try(system(command = categorize_cmd, intern = TRUE))
+    
+    return(data.frame("Time"=as.numeric(parseOutput(categorize_res, "Runtime")),
+                      "In"=parseOutput(categorize_res, "vcf"),
+                      "Out"=parseOutput(categorize_res, "out")))
+  })
+  
+  
+  
+  
 }
---
+++ R/utils.R
@@ -0,0 +1,15 @@
+#' Returns the tag from an execution
+#' 
+#' @description Looks for and parses the
+#' 'Runtime: 25' line in shell executions
+#'
+#' @param results vector of printed statments
+#' @param tag ID tag for the parsed output (e.g. Runtime)
+#'
+#' @return
+#' 
+parseOutput <- function(results, tag){
+  runtime <- results[grep(tag, results, ignore.case = TRUE)]
+  runtime <- gsub("^.*: ", "", runtime)
+  return(runtime)
+}
