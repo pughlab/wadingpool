9c4365d5f59a028339ada6b22269898e1197ed80
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-04 18:27:35 -0500

    output now redirected to tmp/ dir

--- R/genSnpZygosity.R
+++ R/genSnpZygosity.R
@@ -13,8 +13,8 @@
 #' @export
 genSnpZygosity <- function(sampleid, chrs = paste0("chr", c(1:22, "X", "Y"))){
   # sampleid <- 'NET-2-001b_02_T_DNA.processed'
-  sampleid_dir <- gsub("[^a-zA-Z0-9]", "", sampleid)
-  dir.create(sampleid_dir, showWarnings = FALSE)
+  sampleid_dir <- file.path("tmp", gsub("[^a-zA-Z0-9]", "", sampleid))
+  dir.create(sampleid_dir, recursive = TRUE, showWarnings = FALSE)
   
   self_pkg_name <- 'WadingPool'
   bin_dir <- system.file("bin/", package = self_pkg_name)
