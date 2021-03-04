dadca04fb3a24a41326f1ead94c0ef2a45ebdd8e
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-04 15:04:18 -0500

    Have a base system execution of the categorizeAD.sh script

---
+++ R/genSnpZygosity.R
@@ -0,0 +1,25 @@
+#' Title
+#' @description 
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+getSnpZygosity <- function(sampleid, chrs = paste0("chr", c(1:22, "X", "Y"))){
+  sampleid <- 'NET-2-001b_02_T_DNA.processed'
+  
+  sapply(chrs, function(chr){
+    vcf_file <- paste0(chr, sampleid, ".vcf")
+    
+  })
+  
+  
+  self_pkg_name <- 'WadingPool'
+  bin_dir <- system.file("bin/", package = self_pkg_name)
+  conv.cmd <- paste0(bin_dir, "/categorizeAD.sh", 
+                     " -v ", vcf_file, 
+                     " -o ", out_file)
+  ./categorizeAD.sh -v x.vcf -o x_test.vcf
+  conv.cmd.res <- try(system(command = conv.cmd, intern = TRUE))
+}
+genotypeID.R
