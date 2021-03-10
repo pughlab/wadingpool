d74148533b332d8e4ef6e0bd727337521286052a
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-03-10 18:27:01 -0500

    Development copy of the getSampleSimilarity function

---
+++ R/getSampleSimilarity.R
@@ -0,0 +1,42 @@
+
+getSampleSimilarity <- function(filt_dir, samples,
+                       similarityFun=NULL){
+  # similarityFun=NULL
+  filt_files <- list.files(filt_dir, pattern="filt.snps$")
+  
+  jaccmats <- lapply(filt_files, function(f){
+    # f <- filt_files[1]
+    mat <- read.csv(file.path(filt_dir, f), header = FALSE)
+    colnames(mat) <- samples
+    mat[mat==0] <- NA         # Remove no coverage SNPs
+    nonhet_idx <- apply(mat,1,function(i) {
+      i <- na.omit(as.integer(i))
+      any(i==2) | (any(i==1) & any(i==3))
+    })
+    mat[!nonhet_idx,] <- NA    # Removes SNPs that have no het cov
+    
+    if(is.null(similarityFun)){
+      similarityFun <- function(i,j){
+        # Heterozygous SNPs in i and j
+        # Divided by
+        # SNPs heterozygous in i or j + 
+        #   SNPs that are ref in i and alt in j (vice versa)
+        ihet <- i==2
+        jhet <- j==2
+        ihom <- (i==1 | i==3)
+        i[which(ihom)] != j[which(ihom)]
+        
+        sum(ihet & jhet, na.rm=T) / 
+          (sum(ihet | jhet, na.rm=T)) #+ sum(i[which(ihom)] != j[which(ihom)], na.rm=T))
+      }
+    }
+    
+    getN <- function(i,j){ sum(!is.na(i) & !is.na(j)) }
+    
+    simmat <- WadingPool:::allbyall(mat, margin=2, fun=similarityFun)
+    nmat <- WadingPool:::allbyall(mat, margin=2, fun=getN)
+    
+    list("jacc"=simmat,
+         "n"=nmat)
+  })
+}
--- inst/main/main.R
+++ inst/main/main.R
@@ -1,3 +1,6 @@
+devtools::install_github("quevedor2/WadingPool", ref = "dev")
+
+
 library(WadingPool)
 PDIR <- '/mnt/work1/users/pughlab/projects/NET-SEQ/shallow_wgs/variant_calling/mutect_common/output'
 setwd(PDIR)
@@ -6,8 +9,12 @@ files <- list.files(PDIR, pattern="processed.vcf$")
 samples <- unique(gsub("^chr.*?\\.(.*).vcf$", "\\1", files))
 chrs <- paste0("chr", c(1:22, "X", "Y"))
 
-#snp_results <- lapply(samples, genSnpZygosity)
+snp_results <- lapply(samples, genSnpZygosity)
 
 s_paths <- file.path("tmp", gsub("[^a-zA-Z0-9]", "", samples), 
                      paste0("[ID].", samples, "_out.tsv"))
+agg_results <- aggregateAndFilter(s_paths, num_samples=2, dbsnp_file='all.common_all_20151104.bed',
+                                  dbsnp_dir=file.path("~", "pughlab/references/dbsnp/dbsnp_b146_GRCh37p13/common/bed")) 
 
+filt_dir <- file.path("tmp", "combined", "filt")
+getSampleSimilarity(filt_dir, samples)
