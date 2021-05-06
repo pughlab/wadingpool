6009aade8c2ac6a502c68752f575f94dae5bb8b4
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-05-06 15:19:41 -0400

    minor non-relevant fixes

--- build/wp_build.R
+++ build/wp_build.R
@@ -7,13 +7,16 @@ setwd(pkg)
 
 #use_description()
 
+
 #### Assembling data ####
-usethis::use_data_raw()
-devtools::load_all()
+usethis::use_testthat()               # unit testing
+usethis::use_vignette("vignette")  # vignette documentation
+usethis::use_data_raw()               # raw data
+
 
 #### Building ####
 #Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS/pandoc")
-
+usethis::use_package("assertthat")
 usethis::use_package("GenomicRanges")
 usethis::use_package("IRanges")
 usethis::use_package("S4Vectors")
@@ -23,6 +26,7 @@ usethis::use_package("stats")
 usethis::use_package("utils")
 
 
+devtools::load_all()
 devtools::document(pkg)
 devtools::check(pkg)
 devtools::build_vignettes(pkg)
--- inst/external_tools/runGATKCollectAllelicCount.sh
+++ inst/external_tools/runGATKCollectAllelicCount.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-#
-#$ -cwd
+
+#SBATCH -t 5:00 
+#SBATCH --mem=20G 
+#SBATCH -J gatk 
+#SBATCH -p all 
+#SBATCH -c 4 
+#SBATCH -N 1 
+#SBATCH -o %x-%j.out 
+
+PATH=$PATH:/mnt/work1/users/pughlab/bin/conda/anaconda3/bin
+
+export PATH
 chr='all'
 TUMOR="NET-2-011_02_T_DNA.processed"
-dbsnpDir='/mnt/work1/users/pughlab/references/dbsnp/dbsnp_b146_GRCh37p13/common/bed'
+dbsnpDir='/mnt/work1/users/pughlab/references/dbsnp/dbsnp_b146_GRCh37p13/chr_bed'
 dbsnpId='common_all_20151104.bed'
-PDIR='/mnt/work1/users/pughlab/projects/NET-SEQ/shallow_wgs/variant_calling/mutect_common'
+PDIR='/mnt/work1/users/pughlab/projects/NET-SEQ/shallow_wgs/variant_calling/gatk_common'
 
 while getopts 'ct:d:f:p:' flag; do
   case "${flag}" in
