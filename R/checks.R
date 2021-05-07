6bf5d56b9c2680ead75ec0854773b80e608e1537
Rene Quevedo <rquevedo@uhnresearch.ca>
2021-05-07 15:32:56 -0400

    Added a script for assert statements and base for hmm viz

---
+++ R/checks.R
@@ -0,0 +1,15 @@
+.check <- function(x, check, ...){
+  switch(check,
+         plotHMM.est_cols=.chk_est_cols(x, ...))
+}
+
+#########################
+#### hmm_viz.PlotHMM ####
+.chk_est_cols <- function(x, m){
+  assert_that(length(x)==2, is.character(x), all(x %in% colnames(m)),
+              msg="est_cols must be a 2 element character vector for col names in model")
+  assert_that(is.numeric(m[,x[1]]), 
+              msg="est_cols[1] must correspond to a numeric state c(1,2,3,...,n)")
+  assert_that(is.character(m[,x[2]]), 
+              msg="est_cols[2] must correspond to a character label for each state")
+}
